1cf34b018a8c37d090d42285c81556ab5e5a6ae9
MyISmyhome <muafariski282@gmail..com>
2026-01-18 22:21:25 +0700

    a

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "10mb", // Atau '20mb'
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+    ],
   },
 };
 
